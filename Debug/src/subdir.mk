e1f0e4e1277d8423e8b10fc801c331051868225b
sohelmsc <mbhuiyan@ualberta.ca>
2015-05-13 19:53:31 -0600

    Update subdir.mk

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -1,6 +1,3 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
 O_SRCS += \
